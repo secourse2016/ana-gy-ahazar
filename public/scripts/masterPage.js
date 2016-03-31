0edc9499e1cd04ae2633c9827b5472303fc186ec
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-03-31 21:04:28 +0200

    Finished the carousel

--- public/scripts/masterPage.js
+++ public/scripts/masterPage.js
@@ -1,8 +1,16 @@
 $(document).ready(function() {
-   $('.slider').click(function() {
+  var count = $('.slider ul li').size();
+
+   $('#next').click(function() {
+     if(count > 1)
       next();
    });
-   
+
+   $('#previous').click(function() {
+     if(count > 1)
+      previous();
+   });
+
    $('.tab-links a').on('click', function(e)  {
       var currentAttrValue = $(this).attr('href');
       $(this).parent('li').addClass('active').siblings().removeClass('active');
@@ -14,4 +22,12 @@ function next(){
       $(this).find("li:last").after($(this).find('li:first'));
       $(this).css({marginLeft : 0});
    });
+}
+
+function previous(){
+  $('.slider ul').find('li:last').animate({width : 0}, 0, function() {
+    $('.slider ul').find("li:first").before($(this));
+    $('.slider ul').css({marginRight : 0});
+    $(this).animate({width : 1000}, 1000);
+  });
 }
