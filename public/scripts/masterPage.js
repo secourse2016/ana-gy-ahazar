8a592f39b71cff8643df75ae7f12529060a6daf9
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-03-31 21:18:13 +0200

    finished tabs animations

--- public/scripts/masterPage.js
+++ public/scripts/masterPage.js
@@ -1,17 +1,33 @@
+var currentTab = 'book';
+
 $(document).ready(function() {
    $('.slider').click(function() {
       next();
    });
-   
+
    $('.tab-links a').on('click', function(e)  {
-      var currentAttrValue = $(this).attr('href');
       $(this).parent('li').addClass('active').siblings().removeClass('active');
+      currentTab = $(this).attr('href');
+      setTimeout(function() {
+         setHeight();
+      }, 5);
    });
 });
+
+setTimeout(function() {
+   setHeight();
+}, 5);
 
 function next(){
    $('.slider ul').animate({marginLeft : "-=1000px"}, 1000, function() {
       $(this).find("li:last").after($(this).find('li:first'));
       $(this).css({marginLeft : 0});
    });
+}
+
+function setHeight() {
+   console.log('#' + currentTab);
+   var h = $('#' + currentTab).outerHeight();
+   $('.tab-content').animate({height:h+35},500);
+   console.log(currentTab + " " + h);
 }
