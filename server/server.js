357803b145d4e9ec790be43e2b63f020468f6011
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-03-31 14:02:58 +0200

    fixed a fetal bug in server.js that was produced in my last commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public/'));
-app.use(express.static(__dirname + '/public/views/'));
-app.use(express.static(__dirname + '/public/styles/'));
-app.use(express.static(__dirname + '/public/styles/libraries'));
-app.use(express.static(__dirname + '/public/scripts/'));
-app.use(express.static(__dirname + '/public/scripts/controllers'));
-app.use(express.static(__dirname + '/public/scripts/libraries'));
-app.use(express.static(__dirname + '/public/assets/'));
+app.use(express.static(__dirname + '/../public/'));
+app.use(express.static(__dirname + '/../public/views/'));
+app.use(express.static(__dirname + '/../public/styles/'));
+app.use(express.static(__dirname + '/../public/styles/libraries'));
+app.use(express.static(__dirname + '/../public/scripts/'));
+app.use(express.static(__dirname + '/../public/scripts/controllers'));
+app.use(express.static(__dirname + '/../public/scripts/libraries'));
+app.use(express.static(__dirname + '/../public/assets/'));
 
 app.get('/', function(req, res) {
    res.sendFile('index.html');
