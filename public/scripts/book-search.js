06acbdc6da515018fe8a4fa94323b582d2bb614f
sherienfathy <sherienfathy95@gmail.com>
2016-04-03 12:59:34 +0200

    all done except for the country

--- public/scripts/book-search.js
+++ public/scripts/book-search.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-    // console.log('here');
-    $('.datepicker').datepicker({
-      changeMonth: true,//this option for allowing user to select month
-      changeYear: true //this option for allowing user to select from year range
-    });
-
-     $("[id=radio02]").click(function(){
-            $('.RDate').hide();
-            $('.RDLogo').hide();
-            $('.RD').hide();
-           
-    });
-        $("[id=radio01]").click(function(){
-            $('.RDate').show();
-            $('.RDLogo').show();
-            $('.RD').show();
-           
-    });
-
    
- });
+    $('.datepicker').datepicker({
+      changeMonth: true,
+      changeYear: true 
+  });
+
+    $("[id=radio02]").click(function(){
+    	$('.RDate').hide();
+    	$('.RDLogo').hide();
+    	$('.RD').hide();
+
+    });
+    $("[id=radio01]").click(function(){
+    	$('.RDate').show();
+    	$('.RDLogo').show();
+    	$('.RD').show();
+
+    });
+
+
+});
