ee24c08e04662b7744e91d999d7f9bc6691bffa2
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-04-04 22:19:12 +0200

    finished the view without autocomplete

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -6,12 +6,4 @@ App.controller('searchController', function($scope, $http) {
    setTimeout(function() {
       setHeight('book');
    }, 0);
-
-   function AirportCodes() {
-     $http.get('/api/airports').success(function(response) {
-        $scope.Airports = response;
-     });
-   };
-
-   AirportCodes();
 });
