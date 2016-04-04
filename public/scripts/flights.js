1c1e82a5d744dfb044675b7018424e2c1d5e297c
Mariam <mariam.zaky23@gmail.com>
2016-04-04 17:36:57 +0200

    Finished Allgit add --allgit add --all!

--- public/scripts/flights.js
+++ public/scripts/flights.js
@@ -4,25 +4,25 @@ $(document).ready(function () {
 		$(this).addClass('activated');
 	});
 
-	$('.classesTabs a').on('click', function()  {
-    $(this).parent('li').addClass('active').siblings().removeClass('active');
+// 	$('.classesTabs a').on('click', function()  {
+//     $(this).parent('li').addClass('active').siblings().removeClass('active');
     
-    var elID = this.id;
+//     var elID = this.id;
     
-    if(elID == 'class1'){
-    	$('#ticketPrice').text('3,800 EGP');
-    	$('#chairCount').text('5');
-    	$('#ticketRadio').attr( 'disabled', false);
-    }
-    if(elID == 'class2'){
-    	$('#ticketPrice').text('4,260 EGP');
-    	$('#chairCount').text('1');
-    	$('#ticketRadio').attr( 'disabled', false);
-    }
-    if(elID == 'class3'){
-    	$('#ticketPrice').text('5,000 EGP');
-    	$('#chairCount').text('No Seats');
-    	$('#ticketRadio').attr( 'disabled', true);
-    }
-  });
+//     if(elID == 'class1'){
+//     	$('#ticketPrice').text('3,800 EGP');
+//     	$('#chairCount').text('5');
+//     	$('#ticketRadio').attr( 'disabled', false);
+//     }
+//     if(elID == 'class2'){
+//     	$('#ticketPrice').text('4,260 EGP');
+//     	$('#chairCount').text('1');
+//     	$('#ticketRadio').attr( 'disabled', false);
+//     }
+//     if(elID == 'class3'){
+//     	$('#ticketPrice').text('5,000 EGP');
+//     	$('#chairCount').text('No Seats');
+//     	$('#ticketRadio').attr( 'disabled', true);
+//     }
+//   });
 });
