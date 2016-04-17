b0d57f63999b81dd4e080774beeffd5cef42dabd
MohamedEssamEldin <mohamed.essameldin@student.guc.edu.eg>
2016-04-17 14:08:58 +0200

    changed  function attribute in flights.js file

--- app/flights.js
+++ app/flights.js
@@ -4,12 +4,12 @@ var db = require('./db');
  all one way flights  
 */
 
-var getOnewayflights = function(oneWay , callback){
+exports.getOneWayFlights = function(oneWay , callback){
     db.getDatabase().collection('flights').find(oneWay).toArray(function( err , data ){
          if (err){
             	callback(err);
             }else{
-            	callback(null);
+            	callback(null,data);
             }
     }); 
 };
@@ -18,7 +18,7 @@ var getOnewayflights = function(oneWay , callback){
 /*
 The function is used to insert a feedback into Database.
 */
-var addFeedback = function (feed, callback){
+exports.addFeedback = function (feed, callback){
 	db.getDatabase().collection('feedbacks').insert(feed, function(err, docs) {
 		if (err){
             	callback(err);
