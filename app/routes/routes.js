e087455bd18ea83ef80e6e8ed6e9e4dab1cb9d2c
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-04-18 06:26:05 +0200

    added the Middleware for JWT

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,35 +3,40 @@ var db = require('../db');
 
 module.exports = function(app) {
 
+
 	var jwt = require('jsonwebtoken');
 
 	/**
 	* This route returns the master page
-	*
 	*/
 	app.get('/', function(req, res) {
 		res.sendFile('index.html');
 	});
 
-    /**
-	 * This route deletes the database
-	 *
-	 */
-	app.get('/db/delete', function(req, res) {
-    db.clear(function(){
-    	res.send("deleted successfully");
-       });
-   });
-     /**
-	 * This route gets a specific reservation information
-	 *
-	 */
-	 app.get('/api/flights/reservation/:bookingReference', function(req, res) {
-		 flights.getReservation(function(err, data) {
-			 res.json(data);
-		 }, req.params.bookingReference);
-	 });
+	/* Middlewear For Secure API Endpoints */
+	app.use(function(req, res, next) {
 
+		// check header or url parameters or post parameters for token
+		var token = req.body.wt || req.query.wt || req.headers['x-access-token'];
+
+		console.log("{{{{ TOKEN }}}} => ", token);
+
+		var jwtSecret = process.env.JWTSECRET;
+
+		// Get JWT contents:
+		try
+		{
+			var payload = jwt.verify(token, jwtSecret);
+			req.payload = payload;
+			next();
+		}
+		catch (err)
+		{
+			console.error('[ERROR]: JWT Error reason:', err);
+			res.status(403).sendFile('403.html');
+		}
+
+	});
 
 	/**
 	* This route returns a json object with all the countries.
@@ -67,6 +72,26 @@ module.exports = function(app) {
 	});
 
 	/**
+	* This route deletes the database
+	*
+	*/
+	app.get('/db/delete', function(req, res) {
+		db.clear(function(){
+			res.send("deleted successfully");
+		});
+	});
+
+	/**
+	* This route gets a specific reservation information
+	*
+	*/
+	app.get('/api/flights/reservation/:bookingReference', function(req, res) {
+		flights.getReservation(function(err, data) {
+			res.json(data);
+		}, req.params.bookingReference);
+	});
+
+	/**
 	* This route validates the promotion_code
 	*
 	*/
@@ -84,7 +109,7 @@ module.exports = function(app) {
 				var discount = result.discount;
 				db.getDatabase().collection('promotionCodes').remove({"code": promoCode},  function(err, results) {
 					res.send(discount+"");
-					});
+				});
 			} else {
 				res.send(0.0+"");
 			}
