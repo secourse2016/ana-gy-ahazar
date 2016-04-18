624e24485332c555c2d2e436d429be4eaca89121
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-04-18 12:27:11 +0200

    added the middleware to secure the routes using JWT to routes.js.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,10 +1,9 @@
 var flights = require('../flights');
 var db = require('../db');
 var moment = require('moment');
+var jwt = require('jsonwebtoken');
 
 module.exports = function(app) {
-
-	var jwt = require('jsonwebtoken');
 
 	/**
 	* This route returns the master page
@@ -13,25 +12,31 @@ module.exports = function(app) {
 	app.get('/', function(req, res) {
 		res.sendFile('index.html');
 	});
-	/**
-	* This route deletes the database
-	*
-	*/
-	app.get('/db/delete', function(req, res) {
-		db.clear(function(){
-			res.send("deleted successfully");
-		});
-	});
-	/**
-	* This route gets a specific reservation information
-	*
-	*/
-	app.get('/api/flights/reservation/:bookingReference', function(req, res) {
-		flights.getReservation(function(err, data) {
-			res.json(data);
-		}, req.params.bookingReference);
-	});
 
+	/* Middlewear to Secure API Endpoints */
+	app.use(function(req, res, next) {
+
+		// check header or url parameters or post parameters for token
+		var token = req.body.wt || req.query.wt || req.headers['x-access-token'];
+
+		console.log("{{{{ TOKEN }}}} => ", token);
+
+		var jwtSecret = process.env.JWTSECRET;
+
+		// Get JWT contents:
+		try
+		{
+			var payload = jwt.verify(token, jwtSecret);
+			req.payload = payload;
+			next();
+		}
+		catch (err)
+		{
+			console.error('[ERROR]: JWT Error reason:', err);
+			res.status(403).sendFile('403.html');
+		}
+
+	});
 
 	/**
 	* This route returns a json object with all the countries.
@@ -67,6 +72,114 @@ module.exports = function(app) {
 	});
 
 	/**
+	* This route deletes the database
+	*
+	*/
+	app.get('/db/delete', function(req, res) {
+		db.clear(function(){
+			res.send("deleted successfully");
+		});
+	});
+
+	/**
+	* This route searchs for one ways flights.
+	*
+	*/
+	app.get('/api/flights/search/:origin/:destination/:departureDateTime/:classs' , function(req, res){
+
+		var dep_date = new Date(req.params.departureDateTime);
+		dep_date = dep_date.getFullYear() + '' + dep_date.getMonth() + '' + dep_date.getDate();
+
+		var oneWay = {
+			"origin": req.params.origin,
+			"destination": req.params.destination,
+			"departureDate": dep_date
+		};
+		flights.getOneWayFlights(oneWay , function(err ,data){
+			if (err)
+			throw err;
+
+			else
+			res.json(data);
+		});
+	});
+
+	/**
+	* This route searchs for round trip flights.
+	*
+	*/
+	app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:classs', function(req, res) {
+		// retrieve params from req.params.{{origin | departingDate | ...}}
+		// return this exact format
+
+		console.log(req.params);
+		var dep_date = new Date(req.params.departingDate);
+		dep_date = dep_date.getFullYear() + '' + dep_date.getMonth() + '' + dep_date.getDate();
+		var ret_date = new Date(req.params.returningDate);
+		ret_date = ret_date.getFullYear() + '' +ret_date.getMonth() + '' + ret_date.getDate();
+
+		var  outGoing = {
+			"origin":        req.params.origin,
+			"destination":   req.params.destination,
+			"departureDate": dep_date
+		};
+
+		var  inComing = {
+			"origin":        req.params.destination,
+			"destination":   req.params.origin,
+			"departureDate": ret_date
+		};
+
+		var result = {
+			outGoing : {} ,
+			inComing : {}
+		} ;
+		flights.getOneWayFlights(outGoing,function(err ,data ){
+			if(err) throw err ;
+			else{
+				result.outGoing = data ;
+
+				flights.getOneWayFlights(inComing,function(err ,d){
+					if(err) throw err ;
+					result.inComing = d ;
+					res.json(result) ;
+				});
+			}
+		});
+	});
+
+	/**
+	* This route gets a specific reservation information
+	*
+	*/
+	app.get('/api/flights/reservation/:bookingReference', function(req, res) {
+		flights.getReservation(function(err, data) {
+			res.json(data);
+		}, req.params.bookingReference);
+	});
+
+	/**
+	* This route posts a new reservation into the database and returns the booking reference.
+	*
+	*/
+	app.post('/api/flights/reservation', function(req, res) {
+
+		var reservation = req.body;
+		flights.reserve(reservation , function (err){
+			if (err) {
+				res.send("error");
+			}else{
+
+				res.send("success");
+
+			}
+		});
+
+
+	});
+
+
+	/**
 	* This route validates the promotion_code
 	*
 	*/
@@ -90,99 +203,13 @@ module.exports = function(app) {
 		});
 	});
 
-	app.get('/api/flights/search/:origin/:destination/:departureDateTime/:classs' , function(req, res){
-
-		var dep_date = new Date(req.params.departureDateTime);
-		dep_date = dep_date.getFullYear() + '' + dep_date.getMonth() + '' + dep_date.getDate();
-
-		var oneWay = {
-			"origin": req.params.origin,
-			"destination": req.params.destination,
-			"departureDate": dep_date
-		};
-		flights.getOneWayFlights(oneWay , function(err ,data){
-			if (err)
-			throw err;
-
-			else
-			res.json(data);
-
-
-		});
-
-	});
-
-
-	app.get('/api/flights/search/:origin/:destination/:departingDate/:returningDate/:classs', function(req, res) {
-		// retrieve params from req.params.{{origin | departingDate | ...}}
-		// return this exact format
-
-		console.log(req.params);
-		var dep_date = new Date(req.params.departingDate);
-		dep_date = dep_date.getFullYear() + '' + dep_date.getMonth() + '' + dep_date.getDate();
-		var ret_date = new Date(req.params.returningDate);
-		ret_date = ret_date.getFullYear() + '' +ret_date.getMonth() + '' + ret_date.getDate();
-		  console.log(dep_date);
-		  console.log(ret_date);
-		//   res.send('hi');
-
-		var  outGoing = {
-			"origin":        req.params.origin,
-			"destination":   req.params.destination,
-			"departureDate": dep_date
-		};
-
-		var  inComing = {
-			"origin":        req.params.destination,
-			"destination":   req.params.origin,
-			"departureDate": ret_date
-		};
-
-
-
-		var result = {
-			outGoing : {} ,
-			inComing : {}
-		} ;
-		flights.getOneWayFlights(outGoing,function(err ,data ){
-			if(err) throw err ;
-			else{
-				result.outGoing = data ;
-
-				flights.getOneWayFlights(inComing,function(err ,d){
-					if(err) throw err ;
-					result.inComing = d ;
-					res.json(result) ;
-				});
-			}
-
-
-		});
-
-
-
-	});
-
-	app.post('/api/flights/reservation', function(req, res) {
-
-		var reservation = req.body;
-		flights.reserve(reservation , function (err){
-			if (err) {
-				res.send("error");
-			}else{
-
-				res.send("success");
-
-			}
-		});
-
-
-	});
-
+	/**
+	* This route posts a feedback to the database.
+	*
+	*/
 	app.post('/feedback', function( req , res ){
 		var feedback = req.body;
 		flights.addFeedback(feedback , function(err){
-
 			if (err){
 				res.send("error") ;
 			}else{
