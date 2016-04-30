ac1d264b1f71424422be05152ed07a75ac72465d
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-04-30 13:55:49 +0200

    modified index.html and app.js

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,11 +1,11 @@
-// Ionic Starter App
+// Air Madagascar
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'App' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+App = angular.module('App', ['ionic']);
 
-.run(function($ionicPlatform) {
+App.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
