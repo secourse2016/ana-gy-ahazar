201133ba226b554c8641660774f2c1cafbdbfc85
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-05-01 13:03:53 +0200

    finished tabs and header

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -6,19 +6,70 @@
 App = angular.module('App', ['ionic']);
 
 App.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+   $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+         // for form inputs)
+         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+         // Don't remove this line unless you know what you are doing. It stops the viewport
+         // from snapping when text inputs are focused. Ionic handles this internally for
+         // a much nicer keyboard experience.
+         cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+         StatusBar.styleDefault();
+      }
+   });
+});
+
+/*
+Specify the states and set the default state.
+ */
+App.config(function($stateProvider, $urlRouterProvider) {
+   $stateProvider
+
+   .state('tabs', {
+      url: '/tabs',
+      abstract: true,
+      templateUrl: 'templates/tabs.html'
+   })
+
+   .state('tabs.home', {
+      url: '/home',
+      views: {
+         'tabs-home': {
+            templateUrl: 'templates/home.html'
+         }
+      }
+   })
+
+   .state('tabs.book', {
+      url: '/book',
+      views: {
+         'tabs-book': {
+            templateUrl: 'templates/book.html'
+         }
+      }
+   })
+
+   .state('tabs.manage', {
+      url: '/manage',
+      views: {
+         'tabs-manage': {
+            templateUrl: 'templates/manage.html'
+         }
+      }
+   })
+
+   .state('tabs.about', {
+      url: '/about',
+      views: {
+         'tabs-about': {
+            templateUrl: 'templates/about.html'
+         }
+      }
+   });
+
+   $urlRouterProvider.otherwise('/tabs/home');
 });
