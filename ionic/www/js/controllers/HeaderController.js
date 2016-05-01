3bfa490c85597f4a1a9b31cfa27e3f2fe1355d65
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-05-01 17:26:54 +0200

    finished feedback form

--- ionic/www/js/controllers/HeaderController.js
+++ ionic/www/js/controllers/HeaderController.js
@@ -1,6 +1,34 @@
-App.controller('HeaderController', function($scope, $location) {
-   $scope.navigate = function(path) {
-      console.log(path);
-      $location.path(path);
+App.controller('HeaderController', function($scope, $ionicModal, FeedbackSrv) {
+   $scope.data = {};
+
+   // Create and load the Modal
+   $ionicModal.fromTemplateUrl('feedback.html', function(modal) {
+      $scope.feedbackModal = modal;
+   }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+   });
+
+   // This function sends the feedback to the API.
+   $scope.send = function() {
+      var r = FeedbackSrv.storeFeedback($scope.data);
+      if (r) {
+         window.plugins.toast.show("message", "short", "bottom");
+      }
+      // FeedbackSrv.storeFeedback($scope.data).success(function(response) {
+      //    console.log(response);
+      // });
+      $scope.feedbackModal.hide();
+      $scope.data = {};
+   };
+
+    // Open the feedback modal.
+   $scope.open = function() {
+      $scope.feedbackModal.show();
+   };
+
+   // Close the feedback modal.
+   $scope.close = function() {
+      $scope.feedbackModal.hide();
    };
 });
---
+++ ionic/www/js/services/feedbackSrv.js
@@ -0,0 +1,10 @@
+App.factory('FeedbackSrv', function ($http) {
+   return {
+      storeFeedback: function(feedbackData) {
+         // return $http.post('http://54.191.202.17/feedback', feedbackData , {
+         //    "headers" : {'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBaXIgTWFkYWdhc2NhciIsImlhdCI6MTQ2MDk1MDc2NywiZXhwIjoxNDkyNDg2NzcyLCJhdWQiOiI1NC4xOTEuMjAyLjE3Iiwic3ViIjoiQWlyLU1hZGFnYXNjYXIifQ.E_tVFheiXJwRLLyAIsp1yoKcdvb8_xCfhjODqG2QkBI'}
+         // });
+         return true;
+      }
+   };
+});
