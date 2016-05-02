6326a8f0cc16740529bf9a1e144788cfd2ecf4f0
Kareem Mokhtar <k.m.a1_2_3@hotmail.com>
2016-05-02 13:02:22 +0200

    added x-access-token to the allowed headers

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -38,7 +38,7 @@ module.exports = function(app) {
 	app.use(function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', "*");
 		res.header('Access-Control-Allow-Methods­', 'GET,PUT,POST,DELETE');
-		res.header('Access-Control-Allow-Headers­', 'Content-Type');
+		res.header('Access-Control-Allow-Headers­', 'Content-Type, x-access-token');
 		next();
 	});
 
