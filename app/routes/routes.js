60ab57cead97a86f2f139da6662cc77db7979782
root <root@ip-172-31-44-161.us-west-2.compute.internal>
2016-05-02 11:38:11 +0000

    fixed the headers in the middleware

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -37,8 +37,8 @@ module.exports = function(app) {
 	/* Middlewear to allow these API end points to be used by other hosts */
 	app.use(function(req, res, next) {
 		res.header('Access-Control-Allow-Origin', "*");
-		res.header('Access-Control-Allow-Methods­', 'GET,PUT,POST,DELETE');
-		res.header('Access-Control-Allow-Headers­', 'Content-Type, x-access-token');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
 		next();
 	});
 
