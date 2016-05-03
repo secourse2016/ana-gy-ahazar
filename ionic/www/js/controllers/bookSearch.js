4363a695d462a10e2df9588b83c5143e3d6075b9
sherienfathy <sherienfathy95@gmail.com>
2016-05-03 08:54:41 +0200

    some changes

--- ionic/www/js/controllers/bookSearch.js
+++ ionic/www/js/controllers/bookSearch.js
@@ -1,4 +1,4 @@
-App.controller('bookSearch', function($scope,$location){
+App.controller('bookSearch', function($scope,FlightsSrv,$location){
 	
 $(document).ready(function () {
     $(".radio").change(function () { 
@@ -9,5 +9,27 @@ $(document).ready(function () {
         }
     });
 });
+    $scope.from;
+    console.log($scope.from);
+  
+ 
+$scope.trip = "round";
+ $scope.search = function() {
+
+         // FlightsSrv.setSearchOther($scope.search_other);
+         // FlightsSrv.setFlightType($scope.trip_type);
+         FlightsSrv.setSelectedOriginAirport($scope.From);
+         FlightsSrv.setSelectedDestinationAirport($scope.To);
+         FlightsSrv.setDepartureDate($scope.dateValue);
+         FlightsSrv.setReturnDate($scope.dateValue2);
+         FlightsSrv.setAdults($scope.adults);
+         FlightsSrv.setChildren($scope.child);
+         FlightsSrv.setInfants($scope.infant);
+         FlightsSrv.setClass($scope.class);
+console.log(FlightsSrv.getAdults()); 
+
+
+        $location.path('/tabs/flights');        
+    };
 
 });
