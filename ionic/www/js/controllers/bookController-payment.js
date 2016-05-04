78c67431edfba921a504525d57f839f423532de2
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-05-04 16:37:55 +0200

    finished payment view

--- ionic/www/js/controllers/bookController-payment.js
+++ ionic/www/js/controllers/bookController-payment.js
@@ -1,6 +1,31 @@
-App.controller('bookController-payment', function($scope, FlightsSrv, PersonalSrv, promotionSrv, $location, $ionicPopup) {
+App.controller('bookController-payment', function($scope, FlightsSrv, PersonalSrv, promotionSrv, $location, $ionicPopup, ionicDatePicker) {
 
    $scope.paymentData = {};
+
+
+   var ipObj1 = {
+      callback: function (val) {  //Mandatory
+         console.log('Return value from the datepicker popup is : ' + val, new Date(val));
+         $scope.paymentData.expiry_date = val;
+      },
+      from: new Date(), //Optional
+      to: new Date(2030, 12, 31), //Optional
+      showTodayButton: true,
+      todayLabel: 'Today',
+      closeLabel: 'Close',
+      inputDate: new Date(),      //Optional
+      closeOnSelect: true,       //Optional
+      templateType: 'popup'       //Optional
+   };
+
+   $scope.openDatePicker = function(){
+      if ($scope.paymentData.expiry_date) {
+         ipObj1.inputDate = new Date($scope.paymentData.expiry_date);
+      }
+      ionicDatePicker.openDatePicker(ipObj1);
+   };
+
+
    //loading disabled
    $scope.loading = false;
 
@@ -58,100 +83,101 @@ App.controller('bookController-payment', function($scope, FlightsSrv, PersonalSr
 
          console.log('good');
 
-         PersonalSrv.setCardholder($scope.cardholder);
-         PersonalSrv.setMethod($scope.method);
-         PersonalSrv.setCardNumber($scope.card_number);
-         PersonalSrv.setCVS($scope.cvs);
-         PersonalSrv.setExpiryDate($scope.card_expiry_date);
-         PersonalSrv.setBillingCountry($scope.billing_country);
-         PersonalSrv.setBillingCity($scope.billing_city);
-         PersonalSrv.setBillingState($scope.billing_state);
-         PersonalSrv.setZipCode($scope.zip_code);
-         PersonalSrv.setAddress($scope.address);
-         PersonalSrv.setPromotionCode($scope.promotion_code);
+         PersonalSrv.setCardholder($scope.paymentData.card_number);
+         PersonalSrv.setMethod($scope.paymentData.method);
+         PersonalSrv.setCardNumber($scope.paymentData.cardholder);
+         PersonalSrv.setCVS($scope.paymentData.cvs);
+         PersonalSrv.setExpiryDate($scope.paymentData.expiry_date);
+         PersonalSrv.setBillingCountry($scope.paymentData.billing_country);
+         PersonalSrv.setBillingCity($scope.paymentData.billing_city);
+         PersonalSrv.setBillingState($scope.paymentData.billing_state);
+         PersonalSrv.setZipCode($scope.paymentData.zip_code);
+         PersonalSrv.setAddress($scope.paymentData.address);
+         PersonalSrv.setPromotionCode($scope.paymentData.promo_code);
+
+         $scope.paymentData.expiry_date = new Date($scope.paymentData.expiry_date);
 
          FlightsSrv.getPublishableKey(FlightsSrv.getDepartureFlight().IP).success(function(publishableKeyDep) {
-           if(publishableKeyDep === 'not found'){
-             console.log('payment error');
-             $scope.loading = false;
-             $scope.$apply();
-             var alertPopup = $ionicPopup.alert({
-               title: 'Opps...',
-               template: '<center>The booking service on the choosen airline is currently down. Please try again later.</center>'
-             });
-           }
-           else{
-             Stripe.setPublishableKey(publishableKeyDep);
+            if(publishableKeyDep === 'not found'){
+               console.log('payment error');
+               $scope.loading = false;
+               $scope.$apply();
+               var alertPopup = $ionicPopup.alert({
+                  title: 'Opps...',
+                  template: '<center>The booking service on the choosen airline is currently down. Please try again later.</center>'
+               });
+            }
+            else{
+               Stripe.setPublishableKey(publishableKeyDep);
 
-             Stripe.card.createToken({
-               number: $scope.card_number,
-               cvc: $scope.cvs,
-               exp_month: ($scope.card_expiry_date.getMonth() + 1),
-               exp_year: $scope.card_expiry_date.getFullYear()
-             }, function(status, responseDep) {
-               if(responseDep.error){
-                 console.log('payment error');
-                 $scope.loading = false;
-                 sweetAlert("Opps...", responseDep.error.message, "error");
-                 var alertPopup = $ionicPopup.alert({
-                   title: 'Opps...',
-                   template: '<center>' + responseDep.error.message + '.</center>'
-                 });
-                 $scope.$apply();
-               }
-               else{
-                 PersonalSrv.setPaymentTokenDep(responseDep.id);
+               Stripe.card.createToken({
+                  number: $scope.paymentData.card_number,
+                  cvc: $scope.paymentData.cvs,
+                  exp_month: ($scope.paymentData.expiry_date.getMonth() + 1),
+                  exp_year: $scope.paymentData.expiry_date.getFullYear()
+               }, function(status, responseDep) {
+                  if(responseDep.error){
+                     console.log('payment error');
+                     $scope.loading = false;
+                     var alertPopup = $ionicPopup.alert({
+                        title: 'Opps...',
+                        template: '<center>Please enter a valid card information.</center>'
+                     });
+                     $scope.$apply();
+                  }
+                  else{
+                     PersonalSrv.setPaymentTokenDep(responseDep.id);
 
-                 if(FlightsSrv.getFlightType() == "round") {
+                     if(FlightsSrv.getFlightType() == "round") {
 
-                   FlightsSrv.getPublishableKey(FlightsSrv.getReturnFlight().IP).success(function(publishableKeyRet) {
-                     if(publishableKeyRet === 'not found'){
-                       console.log('payment error');
-                       $scope.loading = false;
-                       var alertPopup = $ionicPopup.alert({
-                         title: 'Opps...',
-                         template: '<center>The booking service on the choosen airline is currently down. Please try again later.</center>'
-                       });
-                       $scope.$apply();
+                        FlightsSrv.getPublishableKey(FlightsSrv.getReturnFlight().IP).success(function(publishableKeyRet) {
+                           if(publishableKeyRet === 'not found'){
+                              console.log('payment error');
+                              $scope.loading = false;
+                              var alertPopup = $ionicPopup.alert({
+                                 title: 'Opps...',
+                                 template: '<center>The booking service on the choosen airline is currently down. Please try again later.</center>'
+                              });
+                              $scope.$apply();
+                           }
+                           else{
+                              Stripe.setPublishableKey(publishableKeyRet);
+
+                              Stripe.card.createToken({
+                                 number: $scope.paymentData.card_number,
+                                 cvc: $scope.paymentData.cvs,
+                                 exp_month: ($scope.paymentData.expiry_date.getMonth() + 1),
+                                 exp_year: $scope.paymentData.expiry_date.getFullYear()
+                              }, function(status, responseRet) {
+                                 if(responseRet.error){
+                                    console.log('payment error');
+                                    $scope.loading = false;
+                                    var alertPopup = $ionicPopup.alert({
+                                       title: 'Opps...',
+                                       template: '<center>' + responseDep.error.message + '.</center>'
+                                    });
+                                    $scope.$apply();
+                                 }
+                                 else{
+                                    console.log('payment done');
+                                    PersonalSrv.setPaymentTokenRet(responseRet.id);
+                                    $scope.loading = false;
+                                    $location.path('/tabs/confirmation');
+                                    $scope.$apply();
+                                 }
+                              });
+                           }
+                        });
                      }
                      else{
-                       Stripe.setPublishableKey(publishableKeyRet);
-
-                       Stripe.card.createToken({
-                         number: $scope.card_number,
-                         cvc: $scope.cvs,
-                         exp_month: ($scope.card_expiry_date.getMonth() + 1),
-                         exp_year: $scope.card_expiry_date.getFullYear()
-                       }, function(status, responseRet) {
-                         if(responseRet.error){
-                           console.log('payment error');
-                           $scope.loading = false;
-                           var alertPopup = $ionicPopup.alert({
-                             title: 'Opps...',
-                             template: '<center>' + responseDep.error.message + '.</center>'
-                           });
-                           $scope.$apply();
-                         }
-                         else{
-                           console.log('payment done');
-                           PersonalSrv.setPaymentTokenRet(responseRet.id);
-                           $scope.loading = false;
-                           $location.url('/book/confirmation');
-                           $scope.$apply();
-                         }
-                       });
+                        console.log('payment done');
+                        $scope.loading = false;
+                        $location.path('/tabs/confirmation');
+                        $scope.$apply();
                      }
-                   });
-                 }
-                 else{
-                   console.log('payment done');
-                   $scope.loading = false;
-                   $location.url('/book/confirmation');
-                   $scope.$apply();
-                 }
-               }
-             });
-           }
+                  }
+               });
+            }
          });
       }
       else {
